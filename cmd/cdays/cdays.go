4a2b7addb8c82134099d965d0cec3cbee675ce78
Annika Sirapandji <annika.sirapandji@xing.com>
2018-06-18 15:06:02 +0200

    Add simple router

--- cmd/cdays/cdays.go
+++ cmd/cdays/cdays.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", rootHandler())
-	http.ListenAndServe(":8000", nil)
+	log.Print("The application is starting...")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/home", rootHandler())
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
 func rootHandler() func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-
+		w.Write([]byte("Hello World!"))
 	}
 }
