2f830064a274beb17de4d4101891aa24ec930a09
Annika Sirapandji <annika.sirapandji@xing.com>
2018-06-18 15:36:43 +0200

    Add environment variable

--- cmd/cdays/cdays.go
+++ cmd/cdays/cdays.go
@@ -3,12 +3,18 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/cdays/internal/routing"
 )
 
 func main() {
 	log.Print("The application is starting...")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("The port wasn't set")
+	}
 
 	r := routing.NewBLRouter()
 
